03423b9bff8a2efbf5021ecbec7eb5fa151a3c51
Zimgo012 <rjccabelcina@gmail.com>
2026-04-30 12:05:31 -0400

    feature (config): added websocket configuration

---
+++ src/main/java/com/zimgo/colog/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.zimgo.colog.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Component
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(org.springframework.messaging.simp.config.MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(org.springframework.web.socket.config.annotation.StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+}
