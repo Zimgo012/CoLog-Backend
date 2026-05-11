d052ea0efb67a6eb96a529d5b23b3a6f6a43251c
Zimgo012 <rjccabelcina@gmail.com>
2026-05-11 16:28:08 -0400

    feature (messages): added median for websocket

---
+++ src/main/java/com/zimgo/colog/messages/Messages.java
@@ -0,0 +1,38 @@
+package com.zimgo.colog.messages;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.zimgo.colog.diary.Diary;
+import com.zimgo.colog.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Messages {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+//    private MessageType type;
+
+    private String content;
+
+    private LocalDateTime timestamp;
+
+    @ManyToOne
+    private User sender;
+
+    @ManyToOne
+    @JsonIgnore
+    private Diary diaryId;
+
+
+}
