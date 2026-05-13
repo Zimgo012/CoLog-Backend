75ca119e85fbbcb4e3e6d001f17d7fd448c2eb78
Zimgo012 <rjccabelcina@gmail.com>
2026-05-13 14:56:56 -0400

    feature(MessageController) : added controller for messaging

---
+++ src/main/java/com/zimgo/colog/messages/MessageController.java
@@ -0,0 +1,49 @@
+package com.zimgo.colog.messages;
+
+import com.zimgo.colog.messages.dto.MessageRequest;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class MessageController {
+
+    public MessageService messageService;
+    public SimpMessagingTemplate messagingTemplate;
+
+    public MessageController(MessageService messageService,
+                             SimpMessagingTemplate messagingTemplate) {
+        this.messageService = messageService;
+        this.messagingTemplate = messagingTemplate;
+    }
+
+
+    @MessageMapping("/diary/session/{diaryId}")
+    public void userMessage(@DestinationVariable Long diaryId,
+                            @Payload MessageRequest req) {
+
+            if (req.getType() == null){
+                throw new RuntimeException("Message type is null!");
+            }
+
+            switch (req.getType()) {
+                case DOCUMENT -> messageService.processDocumentMessage(diaryId, req);
+                case CHAT -> messageService.processChatMessage(diaryId,req);
+                case LEAVE -> messageService.processLeaveMessage(diaryId);
+                case JOIN -> messageService.processJoinMessage(diaryId);
+                case FILE -> messageService.processFileMessage(diaryId, req);
+
+                //handler if error here
+                default -> System.out.println("Invalid message type");
+            }
+
+    }
+
+
+}
