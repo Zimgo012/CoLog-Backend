e95acfd77e7f3bc2f63b5486ebda1c03d6b9ee40
Zimgo012 <rjccabelcina@gmail.com>
2026-05-13 14:55:21 -0400

    feature(MessageType) : added message type

---
+++ src/main/java/com/zimgo/colog/messages/MessageType.java
@@ -0,0 +1,17 @@
+package com.zimgo.colog.messages;
+
+public enum MessageType {
+
+    /* Document type message - will update the document inside the diary. used for collaborative editing */
+    DOCUMENT,
+
+    /* Chat type message - will send a message to the diary room */
+
+    CHAT,
+    /* Leave and join message - will notify the diary room  */
+    LEAVE,
+    JOIN,
+
+    /* File type message - will send a file to the diary room*/
+    FILE
+}
