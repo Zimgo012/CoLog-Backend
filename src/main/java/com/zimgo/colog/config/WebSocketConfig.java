5a1fc534729199ffd2f34e462271fe88166c1f57
Zimgo012 <rjccabelcina@gmail.com>
2026-08-17 08:02:50 -0400

    fix (mainRunner config): remove unncessary stuffs from operational transform

--- src/main/java/com/zimgo/colog/config/WebSocketConfig.java
+++ src/main/java/com/zimgo/colog/config/WebSocketConfig.java
@@ -16,7 +16,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(org.springframework.web.socket.config.annotation.StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+//        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173");
     }
 
 }
