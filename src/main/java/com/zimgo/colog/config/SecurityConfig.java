b206bf06390207c0f2e510ef6f055a2250374753
Zimgo012 <rjccabelcina@gmail.com>
2026-08-18 18:18:15 -0400

    feat (security): added xml and config for spring security

---
+++ src/main/java/com/zimgo/colog/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.zimgo.colog.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+    //Security Filter Chain Configuration
+
+
+    //Password Encoder Configuration
+
+    // Authenication Provider Configuration
+
+}
