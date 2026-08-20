d34c5ba92b401d28deaff3b731e458f9d1f41e70
Zimgo012 <rjccabelcina@gmail.com>
2026-08-20 08:33:43 -0400

    fix (security): added in-class dependency so the code will much more readable. also reconfigure filter chain to be compatible with jwt

--- src/main/java/com/zimgo/colog/config/SecurityConfig.java
+++ src/main/java/com/zimgo/colog/config/SecurityConfig.java
@@ -1,34 +1,53 @@
 package com.zimgo.colog.config;
 
-import com.zimgo.colog.auth.CustomUserDetailServices;
+import com.zimgo.colog.auth.security.CustomUserDetailServices;
+import com.zimgo.colog.auth.security.JWTAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    final public CustomUserDetailServices customUserDetailServices;
+    final public JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(CustomUserDetailServices customUserDetailServices, JWTAuthenticationFilter jwtAuthenticationFilter ){
+        this.customUserDetailServices = customUserDetailServices;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
     //Security Filter Chain Configuration
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+
                 //Authorization Config
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/diary/all")
                             .hasRole("USER")
-
+                        .requestMatchers("/auth/**")
+                            .permitAll()
                         .anyRequest()
                             .authenticated()
-                )
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults());
+                );
 
         return http.build();
     }
@@ -51,6 +70,13 @@ public class SecurityConfig {
 
         return provider;
 
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
