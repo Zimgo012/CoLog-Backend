6be0fbf34de0e83a75b026fe98cce988786cf696
Zimgo012 <rjccabelcina@gmail.com>
2026-08-24 12:46:52 -0400

    feat (document) : added new services and controller for document now with auth.

--- src/main/java/com/zimgo/colog/document/DocumentController.java
+++ src/main/java/com/zimgo/colog/document/DocumentController.java
@@ -1,7 +1,11 @@
 package com.zimgo.colog.document;
 
+import com.zimgo.colog.document.dto.DocumentRequest;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/document")
@@ -15,39 +19,65 @@ public class DocumentController {
         this.documentService = documentService;
     }
 
-    @GetMapping("/{diaryId}/all")
-    public ResponseEntity<?> getDiaryDocuments(
-            @PathVariable Long diaryId
-    ) {
+    //CREATE a document
+    @PostMapping("/{diaryId}/")
+    public ResponseEntity<?> createDocument(@PathVariable Long diaryId,
+                                            @RequestBody DocumentRequest req){
+        return ResponseEntity.ok(documentService.createDocument(diaryId,req));
+    }
 
+    //GET all document in that diary
+    @GetMapping("/{diaryId}/all")
+    public ResponseEntity<?> getDiaryDocuments(@PathVariable Long diaryId) {
         return ResponseEntity.ok(
-                documentService.getDiaryDocuments(diaryId)
+                documentService.getAllDocuments(diaryId)
         );
     }
 
-    @GetMapping("/{documentId}")
-    public ResponseEntity<?> getDocument(@PathVariable Long documentId) {
-        return ResponseEntity.ok(documentService.getDocument(documentId));
+    //GET a document
+    @GetMapping("/{diaryId}/{documentId}")
+    public ResponseEntity<?> getDocument(@PathVariable Long diaryId,
+                                         @PathVariable Long documentId) {
+        return ResponseEntity.ok(documentService.getDocument(diaryId, documentId));
     }
 
-    /**
+    //UPDATE a document
+    @PatchMapping("/{diaryId}/{documentId}")
+    public ResponseEntity<?> editDocument(@PathVariable Long diaryId,
+                                          @PathVariable Long documentId,
+                                          @RequestBody DocumentRequest req) throws IOException {
+        return ResponseEntity.ok(documentService.editDocument(diaryId,documentId, req));
+    }
+
+    //DELETE a document
+    @DeleteMapping("/{diaryId}/{documentId}")
+    public ResponseEntity deleteDocument(@PathVariable Long diaryId,
+                                            @PathVariable Long documentId){
+
+        documentService.deleteDocument(diaryId,documentId);
+        return ResponseEntity.ok().build();
+    }
+
+    /** RETRIEVE yjs state
      * Used to update yjs state from server to client
      * @return
      */
-    @GetMapping( "/{documentId}/yjs")
-    public ResponseEntity<?> getState(@PathVariable Long documentId){
-        return ResponseEntity.ok(documentService.getYjsState(documentId));
+    @GetMapping( "/{diaryId}/{documentId}/yjs")
+    public ResponseEntity<?> getState(@PathVariable Long diaryId,
+                                      @PathVariable Long documentId){
+        return ResponseEntity.ok(documentService.getYjsState(diaryId, documentId));
     }
 
-    /**
+    /** SAVE yjs state
      * Used to update yjs state from client to server
      * @return
      */
-    @PostMapping(value = "/{documentId}/yjs",
+    @PostMapping(value = "/{diaryId}/{documentId}/yjs",
             consumes = "application/octet-stream")
-    public ResponseEntity<?> saveState(@PathVariable Long documentId,
+    public ResponseEntity<?> saveState(@PathVariable Long diaryId,
+                                       @PathVariable Long documentId,
                                        @RequestBody byte[] update){
-      documentService.saveYjsState(documentId, update);
+      documentService.saveYjsState(diaryId, documentId, update);
 
       return ResponseEntity.ok().build();
     }
---
+++ src/main/java/com/zimgo/colog/document/dto/DocumentRequest.java
@@ -0,0 +1,16 @@
+package com.zimgo.colog.document.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class DocumentRequest {
+    LocalDateTime date;
+}
