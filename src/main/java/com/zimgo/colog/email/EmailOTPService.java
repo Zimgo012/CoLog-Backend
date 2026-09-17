16116f6036ccc6b3687652d1fa283c94ad29cf2d
Zimgo012 <rjccabelcina@gmail.com>
2026-09-17 11:53:08 -0400

    fix (email) : discovered railway/render doesnt support smtp port 587, so i change to a different provider, brevo, that uses other port

--- src/main/java/com/zimgo/colog/email/EmailOTPService.java
+++ src/main/java/com/zimgo/colog/email/EmailOTPService.java
@@ -1,40 +1,58 @@
 package com.zimgo.colog.email;
 
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class EmailOTPService {
 
-    private final JavaMailSender javaMailSender;
+    private final RestClient restClient;
+    private final String senderEmail;
 
-    @Value("${spring.mail.username}")
-    private String fromEmail;
+    public EmailOTPService(
+            @Value("${brevo.api-key}") String apiKey,
+            @Value("${brevo.sender-email}") String senderEmail
+    ) {
+        this.senderEmail = senderEmail;
 
-    public EmailOTPService(JavaMailSender javaMailSender){
-        this.javaMailSender = javaMailSender;
+        this.restClient = RestClient.builder()
+                .baseUrl("https://api.brevo.com/v3")
+                .defaultHeader("api-key", apiKey)
+                .defaultHeader("accept", "application/json")
+                .defaultHeader("content-type", "application/json")
+                .build();
     }
 
     public void sendOtpEmail(String userEmail, String otpCode) {
 
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setFrom(fromEmail);
-        message.setTo(userEmail);
-        message.setSubject("CoLog - Email Verification");
-
-        message.setText(
+        Map<String, Object> body = Map.of(
+                "sender", Map.of(
+                        "name", "CoLog",
+                        "email", senderEmail
+                ),
+                "to", List.of(
+                        Map.of(
+                                "email", userEmail
+                        )
+                ),
+                "subject", "CoLog - Email Verification",
+                "textContent",
                 "Welcome to CoLog!\n\n" +
                         "Your email verification code is:\n\n" +
                         otpCode + "\n\n" +
                         "This code will expire in 5 minutes.\n\n" +
-                        "If you did not create a CoLog account, you can ignore this email."
+                        "If you did not create a CoLog account, " +
+                        "you can ignore this email."
         );
 
-        javaMailSender.send(message);
+        restClient.post()
+                .uri("/smtp/email")
+                .body(body)
+                .retrieve()
+                .toBodilessEntity();
     }
-
 }
