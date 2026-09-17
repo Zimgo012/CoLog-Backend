3947d88e1fc6970660ce365b6c45c3d95265c86f
Zimgo012 <rjccabelcina@gmail.com>
2026-09-17 12:57:08 -0400

    fix (CORS) : ffixing cors

--- src/main/java/com/zimgo/colog/config/WebSocketConfig.java
+++ src/main/java/com/zimgo/colog/config/WebSocketConfig.java
@@ -31,8 +31,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(org.springframework.web.socket.config.annotation.StompEndpointRegistry registry) {
 //        registry.addEndpoint("/ws").withSockJS();
         registry.addEndpoint("/ws")
-                .setAllowedOrigins(frontendUrl)
-                .setAllowedOriginPatterns("*");;
+                .setAllowedOrigins(frontendUrl);
+//                .setAllowedOriginPatterns("*");
 
     }
 
